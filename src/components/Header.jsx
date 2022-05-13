c495b123ad6eeded0dbbc90c2d97a8e03cdc43a1
Denedo Oghenetega Joseph <tegadev01@gmail.com>
2022-05-13 14:50:53 +0000

    Update site header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function Header() {
   return (
     <div className="fixed z-[999] inset-0 h-[70px] flex py-[14px] shadow-[rgba(0_0_0_0.068)] bg-white shadow-md">
-      <div className="text-[1.3rem] my-auto ml-[22px] font-bold">Where?</div>
+      <div className="text-[1.3rem] my-auto ml-[22px] font-bold">
+        Where am I?
+      </div>
     </div>
   );
 }
